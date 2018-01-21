60582754c9c98922787265b1b23d354198cf7900
Doug DiFilippo <ddifilippo@wespeke.com>
2018-01-21 12:03:01 -0500

    Runs hfmt
    Probably need to make this a commit hook or something

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,28 +1,30 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Lib
-    ( module Lib -- TODO: Exporting everything to make ghci easier, be specific after things are working
-    ) where
+  ( module Lib -- TODO: Exporting everything to make ghci easier, be specific after things are working
+  ) where
 
-import Data.Aeson
-import Data.Proxy
-import GHC.Generics
-import Network.HTTP.Client (newManager)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Servant.API
-import Servant.Client
+import           Data.Aeson
+import           Data.Proxy
+import           GHC.Generics
+import           Network.HTTP.Client     (newManager)
+import           Network.HTTP.Client.TLS (tlsManagerSettings)
+import           Servant.API
+import           Servant.Client
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data StarWarsPerson = StarWarsPerson
-    { name :: String } deriving (Show, Generic)
+  { name :: String
+  } deriving (Show, Generic)
 
 instance FromJSON StarWarsPerson
 
-type StarWarsAPI = "api" :> "people" :> Capture "userid" Int :> Get '[JSON] StarWarsPerson
+type StarWarsAPI
+   = "api" :> "people" :> Capture "userid" Int :> Get '[ JSON] StarWarsPerson
 
 apiInstance :: Proxy StarWarsAPI
 apiInstance = Proxy
@@ -35,8 +37,11 @@ getStarWarsPerson = client apiInstance
 
 run :: IO ()
 run = do
-    manager' <- newManager tlsManagerSettings
-    res <- runClientM (getStarWarsPerson 1) (ClientEnv manager' (BaseUrl Https "swapi.co" 443 ""))
-    case res of
-        Left err -> putStrLn $ "Oh no! Error: " ++ show err
-        Right person -> print person
+  manager' <- newManager tlsManagerSettings
+  res <-
+    runClientM
+      (getStarWarsPerson 1)
+      (ClientEnv manager' (BaseUrl Https "swapi.co" 443 ""))
+  case res of
+    Left err     -> putStrLn $ "Oh no! Error: " ++ show err
+    Right person -> print person
